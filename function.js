4be3c3514b14c0538d861fbc434ac5caa1227e6b
SherazAhmad6167 <chaudhrysheraz10@gmail.com>
2024-11-28 16:37:22 +0500

    Add files via upload

---
+++ function.js
@@ -0,0 +1,28 @@
+/*function userLogin(userName){
+        if(userName===""){
+            console.log("Please enter your name");
+            
+        }else{
+            return `${userName} just loggedIn `;
+        }
+}
+console.log(userLogin("sheraz"))*/
+
+//                          object and functions
+
+/*const user = {
+    name: "sheraz",
+    age : 20
+}
+function userInfo(info){
+    console.log(`your name is ${info.name} and age is ${info.age} years old`);
+}
+userInfo(user)*/
+
+//                         function and Array
+
+const Arr=[1,2,3,4,55]
+function getNum(getArray){
+    console.log(getArray[0]);
+}
+getNum(Arr)
