1ba76de456c4cdb5a9b0cf751679d5c1e25e4b64
SherazAhmad6167 <chaudhrysheraz10@gmail.com>
2024-11-29 12:28:11 +0500

    Add files via upload

---
+++ conditions.js
@@ -0,0 +1,40 @@
+//                              conditions                      //
+// >,<,==, >=,          " ===  "  is used for comparison and to check type....
+//if(2==="2"){
+  //  console.log("executed"); }   
+
+
+//                  switch          //
+/*let day = "wed"
+switch(day){
+    case("mon"):
+    console.log("monday");
+    break;
+    case("tues"):
+    console.log("tuesday");
+    break;
+    case("wed"):
+    console.log("wednesday");
+    break;
+    default:
+    console.log("not matched");
+    break; 
+}*/
+
+
+// falsy value:  0, false ,-0, "" , NaN , undefined, null
+// truly value: "  ", [], "0", 'false', {}, function(){}
+
+
+//              Ternary operator
+// const price = 10;
+// price <= 5 ? console.log("true") : console.log("false");
+
+//                      for and while               //
+for (let i=0; i<5; i++){
+
+    console.log("*");
+    console.repeat("*")
+    
+    
+}
