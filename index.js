e0a4b246c08e4883b1aeaa2f66b470235a6d5985
SherazAhmad6167 <chaudhrysheraz10@gmail.com>
2024-12-09 13:25:08 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,39 +1,7 @@
-/*console.log("Sheraz");
+import dotenv from "dotenv"
+import connectDB from "./db/db.js";
+dotenv.config({
+    path :'./.env'
+})
 
-const id=10;
-let name="alli";
-let city="lahore";
-let number= 9823;
-//console.log(id);
-//console.table([id,name,city,number]);
-console.log(typeof city);*/
-
-/*let String = "10";
-let stringOfNumber = Number(String);
-console.log(stringOfNumber);
-console.log(typeof stringOfNumber);*/
-/*
-console.log(null>0);
-console.log(null==0);
-console.log(null>=0);*/
-
-/*let int= Symbol(2);
-let tInt =Symbol(2);
-console.log(int==tInt);*/
-
-//stack ---- premitive
-//heap ----- non-premitive
-
-/*let name= "  sheraz  ";
-let age= 20;
-//console.log(`my name is ${name} and my age is ${age} years old`);
-console.log(name.indexOf('h'),name);
-console.log(name.trim());*/
-
-/*let myDate = new Date();
-console.log(myDate.toJSON());*/
-
-
-
-
-
+connectDB();
